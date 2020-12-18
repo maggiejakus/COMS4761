fa2fcf200d8dd0df16ea4a01118f9e9007edabc3
maggiejakus <59844686+maggiejakus@users.noreply.github.com>
2020-12-18 09:10:56 -0500

    update for ReadME

--- plotting_diseases.R
+++ plotting_diseases.R
@@ -4,14 +4,14 @@ library(cowplot)
 library(reshape2)
 theme_set(theme_cowplot())
 
-PCA <- read.table("PCA_components.lopez-isac.to_plot.gz")
-weights <- read.table("weights.lopez-isac.to_plot.gz")
-means <- read.table("means.lopez-isac.to_plot.gz")
-probs <- read.table("probabilities.lopez-isac.to_plot.gz")
-rsids <- read.table("pca_rsids.lopez-isac.to_plot.gz", stringsAsFactors = F)
-#supersnps <- read.table("superclump.snps.lopez-isac.to_plot", stringsAsFactors = F)
-annot <- read.table("annot.lopez-isac.to_plot.gz")
-annot_names <- read.table("annot_names.lopez-isac.to_plot.gz", stringsAsFactors = F)
+PCA <- read.table("PCA_components.mahajan.to_plot.gz")
+weights <- read.table("weights.mahajan.to_plot.gz")
+means <- read.table("means.mahajan.to_plot.gz")
+probs <- read.table("probabilities.mahajan.to_plot.gz")
+rsids <- read.table("pca_rsids.mahajan.to_plot.gz", stringsAsFactors = F)
+#supersnps <- read.table("superclump.snps.mahajan.to_plot", stringsAsFactors = F)
+annot <- read.table("annot.mahajan.to_plot.gz")
+annot_names <- read.table("annot_names.mahajan.to_plot.gz", stringsAsFactors = F)
 
 for_plot <- data.frame(pc1 = PCA$V1, pc2 = PCA$V2, pc3 = PCA$V3, pc4 = PCA$V4,
                        prob = probs[,1], super_snp = 1)
@@ -42,15 +42,15 @@ for_plot <- data.frame(pc1 = PCA$V1, pc2 = PCA$V2, pc3 = PCA$V3, pc4 = PCA$V4,
 #final_weights <- read.table("total_weights.to_plot.gz", stringsAsFactors = F)
 #PGS_annot <- read.table("PGS_annot.to_plot.gz", stringsAsFactors = F)
 
-final_weights <- read.table("total_weights.lopez-isac.to_plot.gz", stringsAsFactors = F)
-PGS_annot <- read.table("PGS_annot_sums.lopez-isac.to_plot.gz", stringsAsFactors = F)
-superclump_annot <- read.table("super_sums.lopez-isac.to_plot.gz", stringsAsFactors = F)
+final_weights <- read.table("total_weights.mahajan.to_plot.gz", stringsAsFactors = F)
+PGS_annot <- read.table("PGS_annot_sums.mahajan.to_plot.gz", stringsAsFactors = F)
+superclump_annot <- read.table("super_sums.mahajan.to_plot.gz", stringsAsFactors = F)
 
 #PGS_annot_second <- read.table("PGS_annot_sums.to_plot.gz", stringsAsFactors = F)
 
-PGS_diff <- read.table("PGS_diff.lopez-isac.to_plot.gz", stringsAsFactors = F)
-gmm_diff <- read.table("gmm_diff.lopez-isac.to_plot.gz", stringsAsFactors = F)
-super_diff <- read.table("super_diff.lopez-isac.to_plot.gz", stringsAsFactors = F)
+PGS_diff <- read.table("PGS_diff.mahajan.to_plot.gz", stringsAsFactors = F)
+gmm_diff <- read.table("gmm_diff.mahajan.to_plot.gz", stringsAsFactors = F)
+super_diff <- read.table("super_diff.mahajan.to_plot.gz", stringsAsFactors = F)
 
 final_weights <- as.data.frame(t(final_weights))
 colnames(final_weights) <- c("Annotation", "Nonrisky", "Risky")
@@ -103,15 +103,15 @@ ggplot(diff_mtx, aes(y = nice_anno, x = value, color = variable)) +
 
 #############################################################################
 #this tells whether nonrisky vs risky gmm annots are different from each other
-nonrisky_gmm <- read.table('gmm_nonrisky.ttest.lopez-isac.to_plot.gz', stringsAsFactors = F)
-risky_gmm <- read.table('gmm_risky.ttest.lopez-isac.to_plot.gz', stringsAsFactors = F)
+nonrisky_gmm <- read.table('gmm_nonrisky.ttest.mahajan.to_plot.gz', stringsAsFactors = F)
+risky_gmm <- read.table('gmm_risky.ttest.mahajan.to_plot.gz', stringsAsFactors = F)
 gmm_tstats <- rep(0, ncol(risky_gmm))
 for(i in 1:ncol(risky_gmm)){
   gmm_tstats[i] <- t.test(nonrisky_gmm[nonrisky_gmm[,i] != 0,i], risky_gmm[risky_gmm[,i] != 0, i])$stat
 }
 
-nonrisky_superclump <- read.table('super_nonrisky.ttest.lopez-isac.to_plot.gz', stringsAsFactors = F)
-risky_superclump <- read.table('super_risky.ttest.lopez-isac.to_plot.gz', stringsAsFactors = F)
+nonrisky_superclump <- read.table('super_nonrisky.ttest.mahajan.to_plot.gz', stringsAsFactors = F)
+risky_superclump <- read.table('super_risky.ttest.mahajan.to_plot.gz', stringsAsFactors = F)
 super_tstats <- rep(0, ncol(risky_superclump))
 for(i in 1:ncol(risky_superclump)){
   if((length(unique(risky_superclump[,i])) > 1)  & (length(unique(nonrisky_superclump[,i]))>1)){
@@ -119,8 +119,8 @@ for(i in 1:ncol(risky_superclump)){
   }
 }
 
-nonrisky_PGS <- read.table('PGS_nonrisky.ttest.lopez-isac.to_plot.gz', stringsAsFactors = F)
-risky_PGS <- read.table('PGS_risky.ttest.lopez-isac.to_plot.gz', stringsAsFactors = F)
+nonrisky_PGS <- read.table('PGS_nonrisky.ttest.mahajan.to_plot.gz', stringsAsFactors = F)
+risky_PGS <- read.table('PGS_risky.ttest.mahajan.to_plot.gz', stringsAsFactors = F)
 PGS_tstats <- rep(0, ncol(risky_PGS))
 for(i in 1:ncol(risky_PGS)){
   if((length(unique(risky_PGS[,i])) > 1)  & (length(unique(nonrisky_PGS[,i]))>1)){
@@ -128,6 +128,6 @@ for(i in 1:ncol(risky_PGS)){
   }
 }
 
-write.csv(PGS_tstats, "lopez-isac_PGStstat.csv")
-write.csv(super_tstats, "lopez-isac_supertstat.csv")
-write.csv(gmm_tstats, "lopez-isac_gmmtstat.csv")
+write.csv(PGS_tstats, "mahajan_PGStstat.csv")
+write.csv(super_tstats, "mahajan_supertstat.csv")
+write.csv(gmm_tstats, "mahajan_gmmtstat.csv")
