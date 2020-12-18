cc72fa0846e6e66d9f3b785a37a1c43b98176f03
maggiejakus <59844686+maggiejakus@users.noreply.github.com>
2020-12-18 09:07:33 -0500

    updating for ReadME

--- score_and_cluster.py
+++ score_and_cluster.py
@@ -6,7 +6,7 @@ from sklearn import decomposition
 from sklearn import mixture
 #import mca
 
-author = "malik"
+author = "mahajan"
 
 
 #annotations file - converting alkes grch38 to grch37
@@ -17,15 +17,10 @@ mich = pd.read_csv("converted_annot.txt", delimiter = ' ')
 super_snps = pd.read_csv("superclump.snps."+author+".to_plot")
 
 
-#confused about functionality..
-#import PGS
-
-## MALIK PGS doesn't have BP POS - using RSID instead
-PGS = pd.read_csv("PGS000038.txt", delimiter = '\t', comment='#')
-PGS_snps = PGS.iloc[:, 0].to_numpy() #PGS BP POS LOCATION
+PGS = pd.read_csv("PGS000036.txt", delimiter = '\t', comment='#')
+PGS_snps = PGS.iloc[:, 0].to_numpy() #PGS rsID LOCATION
 
 #pdb.set_trace()
-#mich is annotations for cols 14 onward
 annot_names = np.array(list(mich)[14:])
 annot = mich.iloc[:,14:].to_numpy()
 
@@ -34,9 +29,8 @@ annot = annot[:,["extend" in x for x in annot_names]]
 annot_names = annot_names[["extend" in x for x in annot_names]]
 
 #identify values in mich
-#THESE VALUES CHANGE W DIFF GWAS
 bp_pos = mich.iloc[:,1].to_numpy()
-beta = (mich.iloc[:,6]).to_numpy() #NOTE - used LOG OF ODDS RATIO FOR PHELAN
+beta = (mich.iloc[:,6]).to_numpy() 
 pval_weight = np.log10(mich.iloc[:,7].to_numpy()) * -1.0
 mich = mich.iloc[:,2].to_numpy() #this is rsID
 
@@ -67,9 +61,6 @@ ss_PCA = ss_PCA[ss_PCA[:,0] < np.quantile(ss_PCA[:,0], [0.999]), :]
 explained_variance = pca.explained_variance_ratio_
 
 #calculate functional annotation scores for PGS
-
-#pdb.set_trace()
-### ~~~!!!!
 bool_PGS = np.in1d(mich, PGS_snps) #make sure this makes sense
 PGS_annot = annot[bool_PGS, :]
 PGS_weights = np.sum(PGS_annot, axis = 0)
@@ -85,8 +76,6 @@ PGS_nonrisky_vec = np.vstack((annot_names, PGS_nonrisky_weights))
 PGS_diff = [float(PGS_nonrisky_vec[1][i]) - float(PGS_vec[1][i]) for i in range(len(PGS_vec[1]))]
 
 #calculate functional annotation scores for superclump
-
-#pdb.set_trace()
 super_snps = super_snps.to_numpy()[:,0]
 bool_super = np.in1d(mich, super_snps) #CHANGE HERE
 super_annot = annot[bool_super, :]
